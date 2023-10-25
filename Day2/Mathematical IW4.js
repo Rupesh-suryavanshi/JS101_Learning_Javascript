6fdbaf710df11efe3ca73ca07df53534b2214a15
Rupesh-suryavanshi <rupeshsuryavanshi1008@gmail.com>
2023-10-25 13:19:10 +0530

    day2 task  completed

---
+++ Day2/Mathematical IW4.js
@@ -0,0 +1,17 @@
+var initialUsername = "Rupesh";
+var initialPassword = "cat144";
+
+var username = "Rupesh";
+var password = "cat144";
+
+if (initialUsername == username) {
+  if (initialPassword == password) {
+    console.log("log in");
+  }
+  else {
+    console.log("Password missmatch");
+  }
+}
+else {
+  console.log("Username missmatch");
+}
